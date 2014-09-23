4687701ced275f65f81585714cfb5906af4d8748
Ulices <ulices.barajas@crowdint.com>
2014-09-23 13:31:04 -0500

    Add checkout create endpoint

---
+++ app/decorators/controllers/spree/checkouts_controller_decorator.rb
@@ -0,0 +1,57 @@
+Spree::Api::CheckoutsController.class_eval do
+
+  def create
+    compouse_order
+
+    authorize! :update, @order, order_token
+
+    unless order_next_state
+      invalid_resource!(@order)
+    end
+
+    respond_with(@order, default_template: 'spree/api/orders/show') if @order.completed?
+  end
+
+  private
+
+  def add_order_line_items
+    params[:order][:line_items].each do |item|
+      variant = Spree::Variant.find(item[:variant_id])
+      @order.contents.add(variant, item[:quantity] || 1)
+    end
+  end
+
+  def compouse_order
+    authorize! :create, Spree::Order
+    order_user = Spree.user_class.find(params[:user_id])
+
+    @order = Spree::Core::Importer::Order.import(order_user, {})
+    params[:id] = @order.to_param
+    params[:order_token] = @order.guest_token
+  end
+
+  def order_next_state
+    return true if @order.completed?
+
+    return false unless @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+
+    if current_api_user.has_spree_role?('admin') && user_id.present?
+      @order.associate_user!(Spree.user_class.find(params[:user_id]))
+    end
+
+    add_order_line_items if @order.cart?
+
+    after_update_attributes
+
+    if @order.next
+      state_callback(:after)
+      order_next_state
+    else
+      false
+    end
+  end
+
+  def skip_state_validation?
+    true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
+
+  Spree::Core::Engine.add_routes do
+    namespace :api do
+      resources :checkouts, only: [:create], format: 'json'
+    end
+  end
 end

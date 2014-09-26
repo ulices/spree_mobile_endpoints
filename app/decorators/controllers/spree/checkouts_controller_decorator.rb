0b7f7b98d7c5a5e6fa79f8540dc62bb80a3f2ed5
Ulices <ulices.barajas@crowdint.com>
2014-09-26 12:09:23 -0500

    Removing scheluded attributes, this is handled by spree_food_menu

--- app/decorators/controllers/spree/checkouts_controller_decorator.rb
+++ app/decorators/controllers/spree/checkouts_controller_decorator.rb
@@ -18,10 +18,7 @@ Spree::Api::CheckoutsController.class_eval do
   def add_order_line_items
     params[:order][:line_items].each do |item|
       variant = Spree::Variant.find(item[:variant_id])
-      line_item = @order.contents.add(variant, item[:quantity] || 1)
-      line_item.delivery_type = item[:delivery_type]
-      line_item.delivery_time = item[:delivery_time]
-      line_item.save!
+      @order.contents.add(variant, item[:quantity] || 1)
     end
   end
 

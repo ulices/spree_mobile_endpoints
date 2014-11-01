86301de93553445042f04205457272b53ceb1655
Isay Sosa <yasnet21@gmail.com>
2014-11-01 00:13:03 -0600

    Overriding checkout flow

--- app/decorators/models/spree/order_decorator.rb
+++ app/decorators/models/spree/order_decorator.rb
@@ -1,9 +1,13 @@
 Spree::Order.class_eval do
-  def confirmation_required?
-    true
+  checkout_flow do
+    go_to_state :address
+    go_to_state :delivery
+    go_to_state :payment, if: ->(order) { order.payment_required? }
+    go_to_state :confirm, if: ->(order) { order.confirmation_required? }
+    go_to_state :complete
   end
 
-  def payment_required?
+  def confirmation_required?
     true
   end
 end
